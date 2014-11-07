eeaba017a066b223b06fe15b790f9f2dcf17b8c9
carusocr <carusocr@ldc.upenn.edu>
2014-11-07 15:41:53 -0500

    finished regexes

--- regular-expressions-advanced/lib/phone_number_formatter.rb
+++ regular-expressions-advanced/lib/phone_number_formatter.rb
@@ -4,6 +4,11 @@ class PhoneNumberFormatter
   end
 
   def format_number
-    @number.sub(/.*(\d{3}).*(\d{3}).*(\d{4}).*/,'(\1) \2-\3')
+    intlcode = @number[/(\+\d)/]
+    if intlcode
+      @number.sub(/.*?(\+\d)(\d{3}).*(\d{3}).*(\d{4}).*/,'\1 (\2) \3-\4')
+    else
+      @number.sub(/.*(\d{3}).*(\d{3}).*(\d{4}).*/,'(\1) \2-\3')
+    end
   end
 end
